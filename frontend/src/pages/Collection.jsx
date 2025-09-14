be68f5e152209622ae81f9a53a52e6a5a9cb7bb1
Vaibhav Vats <vats88690@gmail.com>
2025-09-14 21:25:40 +0530

    add collection code

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import ProductItem from '../components/ProductItem'
+import { ShopContext } from '../context/ShopContext'
 
 const Collection = () => {
+  const {products} =useContext(ShopContext)
+  
   return (
-    <div>Collection</div>
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+      {products.map((item,index)=>{<ProductItem key={index} image={item.image} name={item.name} price={item.price} />})}
+    </div>
+    
   )
 }
 
